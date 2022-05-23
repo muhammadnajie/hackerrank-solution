6fe3bb689ff00a7fb1d85b66914c21911a4da373
rs-yz <rsyzark@gmail.com>
2022-05-23 11:19:44 +0700

    problem: the love letter mystery, with go

---
+++ problem-solving/the-love-letter-mystery/solution.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'theLoveLetterMystery' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+func theLoveLetterMystery(s string) int32 {
+    var steps int32 = 0;
+    
+    for i := 0; i < len(s) / 2; i++ {
+        leftSide := s[i]
+        rightSide := s[len(s)-i-1]
+        
+        if leftSide != rightSide {
+            steps += absDiff(rune(leftSide), rune(rightSide))
+        }
+    }
+    
+    return steps;
+}
+
+func absDiff(a, b rune) int32 {
+    return int32(math.Abs(float64(a-b)))
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    qTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+    q := int32(qTemp)
+
+    for qItr := 0; qItr < int(q); qItr++ {
+        s := readLine(reader)
+
+        result := theLoveLetterMystery(s)
+
+        fmt.Fprintf(writer, "%d\n", result)
+    }
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
